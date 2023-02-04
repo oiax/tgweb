dd10ac21205827a87640a3095e5c3a0ac275b840
Tsutomu Kuroda <t-kuroda@oiax.jp>
2023-02-04 11:42:56 +0900

    Regenerate articles

--- test/tgweb/update.test.mjs
+++ test/tgweb/update.test.mjs
@@ -1,4 +1,5 @@
 import assert from "node:assert/strict"
+import create from "../../lib/tgweb/create.mjs"
 import update from "../../lib/tgweb/update.mjs"
 import { getSiteData } from "../../lib/tgweb/get_site_data.mjs"
 import { fileURLToPath } from "url";
@@ -54,6 +55,26 @@ describe("update", () => {
 
     const html = fs.readFileSync(wd + "a/dist/index.html")
 
+    assert.match(html.toString(), /\bgreat\b/)
+  })
+
+  it("should regenerate an article dependent on a component", () => {
+    const wd = PATH.resolve(__dirname, "../examples/site_1")
+    process.chdir(wd)
+    fs.rmSync(wd + "/dist", { force: true, recursive: true })
+    const siteData = getSiteData(wd)
+    create("src/articles/about.html", siteData)
+
+    process.chdir(wd + "a")
+
+    update("src/articles/about.html", siteData)
+    update("src/components/hello.html", siteData)
+
+    assert.equal(fs.existsSync(wd + "a/dist/articles/about.html"), true)
+
+    const html = fs.readFileSync(wd + "a/dist/articles/about.html")
+
+    assert.match(html.toString(), /\bAbout us\b/)
     assert.match(html.toString(), /\bgreat\b/)
   })
 })
