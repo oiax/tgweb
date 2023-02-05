cd2b36bb17b6b3b6335a3743de5ae9760c1b5e69
Tsutomu Kuroda <t-kuroda@oiax.jp>
2023-02-05 17:57:37 +0900

    chdir to the specified directory before starting the server

--- packages/tgweb/lib/server.mjs
+++ packages/tgweb/lib/server.mjs
@@ -1,14 +1,33 @@
 #! /usr/bin/env node
 
+import * as PATH from "path"
+import fs from "fs"
 import express from "express"
 import http from "http"
 import reload from "reload"
 import chokidar from "chokidar"
-import router from "./server/router.mjs"
+import { getRouter } from "./server/router.mjs"
 import tgweb from "./tgweb.mjs"
 
 const run = () => {
+  const targetDirName = process.argv[2]
+
+  if (targetDirName == undefined) {
+    console.log("Usage: npx tgweb-server <site-name>")
+    return
+  }
+
+  const targetDirPath = PATH.resolve(process.cwd(), `./sites/${targetDirName}`)
+
+  if (!fs.existsSync(targetDirPath)) {
+    console.log(`A directory named "sites/${targetDirName}" does not exist.`)
+    return
+  }
+
+  process.chdir(targetDirPath)
+
   const app = express()
+  const router = getRouter()
 
   app.set("port", process.env.PORT || 3000)
   app.use(router)
--- packages/tgweb/lib/server/router.mjs
+++ packages/tgweb/lib/server/router.mjs
@@ -1,27 +1,31 @@
 import express from "express"
 import path from "path"
 
-const router = express.Router()
-const documentRoot = path.join(process.cwd(), "dist")
+const getRouter = () => {
+  const router = express.Router()
+  const documentRoot = path.join(process.cwd(), "dist")
 
-router.get("/", function (req, res) {
-  res.sendFile(path.join(documentRoot, "index.html"))
-})
+  router.get("/", function (req, res) {
+    res.sendFile(path.join(documentRoot, "index.html"))
+  })
 
-router.get("/*.html", function (req, res) {
-  res.sendFile(path.join(documentRoot, req.path))
-})
+  router.get("/*.html", function (req, res) {
+    res.sendFile(path.join(documentRoot, req.path))
+  })
 
-router.get("/css/tailwind.css", function (req, res) {
-  res.sendFile(path.join(documentRoot, req.path))
-})
+  router.get("/css/tailwind.css", function (req, res) {
+    res.sendFile(path.join(documentRoot, req.path))
+  })
 
-router.get(/^\/images\/.*\.(gif|png|jpe?g|svg|avif)$/, function (req, res) {
-  res.sendFile(path.join(documentRoot, req.path))
-})
+  router.get(/^\/images\/.*\.(gif|png|jpe?g|svg|avif)$/, function (req, res) {
+    res.sendFile(path.join(documentRoot, req.path))
+  })
 
-router.get(/^\/audios\/.*\.(mp3|wav)$/, function (req, res) {
-  res.sendFile(path.join(documentRoot, req.path))
-})
+  router.get(/^\/audios\/.*\.(mp3|wav)$/, function (req, res) {
+    res.sendFile(path.join(documentRoot, req.path))
+  })
 
-export default router
+  return router
+}
+
+export { getRouter }
