c728a69f43e78b1206e3aa852bbdca1336a873b0
Tsutomu Kuroda <t-kuroda@oiax.jp>
2023-02-05 17:32:05 +0900

    Refactor tgweb server

--- packages/tgweb/lib/server.mjs
+++ packages/tgweb/lib/server.mjs
@@ -7,27 +7,31 @@ import chokidar from "chokidar"
 import router from "./server/router.mjs"
 import tgweb from "./tgweb.mjs"
 
-const app = express()
+const run = () => {
+  const app = express()
 
-app.set("port", process.env.PORT || 3000)
-app.use(router)
+  app.set("port", process.env.PORT || 3000)
+  app.use(router)
 
-const server = http.createServer(app)
+  const server = http.createServer(app)
 
-reload(app).then(function (reloadReturned) {
-  chokidar.watch("./dist").on("all", (_event, _path) => {
-    reloadReturned.reload()
+  reload(app).then(function (reloadReturned) {
+    chokidar.watch("./dist").on("all", (_event, _path) => {
+      reloadReturned.reload()
+    })
+
+    server.listen(app.get("port"), function () {
+      console.log("Web server listening on port " + app.get("port"))
+    })
+  }).catch(function (err) {
+    console.error("Could not start a web server.", err)
   })
 
-  server.listen(app.get("port"), function () {
-    console.log("Web server listening on port " + app.get("port"))
-  })
-}).catch(function (err) {
-  console.error("Could not start a web server.", err)
-})
+  const siteData = tgweb.getSiteData(process.cwd())
 
-const siteData = tgweb.getSiteData(process.cwd())
+  chokidar.watch("./src")
+    .on("add", path => tgweb.create(path, siteData))
+    .on("change", path => tgweb.update(path, siteData))
+}
 
-chokidar.watch("./src")
-  .on("add", path => tgweb.create(path, siteData))
-  .on("change", path => tgweb.update(path, siteData))
+run()
