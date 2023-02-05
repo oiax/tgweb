3b1d5a9f39808e8c18cf990d8d11f2721035e07d
Tsutomu Kuroda <t-kuroda@oiax.jp>
2023-02-05 09:33:16 +0900

    Impl. a primitive create-tgweb binary

---
+++ packages/create-tgweb/create-tgweb.mjs
@@ -0,0 +1,22 @@
+#!/usr/bin/env node
+import * as PATH from "path"
+import glob from "glob"
+import fs from "fs"
+import { fileURLToPath } from "url"
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = PATH.dirname(__filename);
+const targetDir = process.cwd()
+
+const run = () => {
+  console.log("RUN")
+  const src = PATH.resolve(__dirname, "./templates")
+
+  glob.sync(`${src}/*`).map(path => {
+    const filename = PATH.basename(path)
+    const dest = PATH.resolve(targetDir, `./${filename}`)
+    fs.copyFileSync(path, dest)
+  })
+}
+
+run()
