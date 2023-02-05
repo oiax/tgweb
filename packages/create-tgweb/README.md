d80ada08b6c03440b3721ce1a889ecb91a3257db
Tsutomu Kuroda <t-kuroda@oiax.jp>
2023-02-05 13:57:14 +0900

    create-tgweb: mkdir before copying files

--- packages/create-tgweb/README.md
+++ packages/create-tgweb/README.md
@@ -3,8 +3,23 @@
 ## Scaffolding for tgweb
 
 ```bash
-npm create tgweb
+npm create tgweb@latest
+```
+
+This command creates a directory named `tgweb` in the _current_ directory.
+
+If you want to create a directory with another name, specify it as an argument as follows:
+
+```bash
+npm create tgweb@latest our_web
+```
+
+## Installation of tgweb
+
+```
+cd tgweb
 npm install
 ```
 
-This command creates a `package.json` in the _current_ directory.
+For more information on how to create websites with _tgweb_,
+please visit its [page](https://www.npmjs.com/package/tgweb) on npmjs.com.
--- packages/create-tgweb/create-tgweb.mjs
+++ packages/create-tgweb/create-tgweb.mjs
@@ -6,16 +6,26 @@ import { fileURLToPath } from "url"
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = PATH.dirname(__filename);
-const targetDir = process.cwd()
 
 const run = () => {
   const src = PATH.resolve(__dirname, "./templates")
+  const targetDirName = process.argv[2] || "tgweb"
+  const targetDirPath = PATH.resolve(process.cwd(), `./${targetDirName}`)
+
+  if (fs.existsSync(targetDirPath)) {
+    console.log(`A directory named ${targetDirName} already exists.`)
+    return
+  }
+
+  fs.mkdirSync(targetDirPath)
 
   glob.sync(`${src}/*`).map(path => {
     const filename = PATH.basename(path)
-    const dest = PATH.resolve(targetDir, `./${filename}`)
+    const dest = PATH.resolve(targetDirPath, `./${filename}`)
     fs.copyFileSync(path, dest)
   })
+
+  console.log("Tgweb was installed successfully.")
 }
 
 run()
