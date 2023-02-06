96badd97ba14c23753c6886f2d3e2bf7e64fe732
Tsutomu Kuroda <t-kuroda@oiax.jp>
2023-02-06 10:09:44 +0900

    Fix get_type.mjs to handle articles correctly

--- packages/tgweb/lib/tgweb/get_type.mjs
+++ packages/tgweb/lib/tgweb/get_type.mjs
@@ -2,6 +2,7 @@ const getType = function(dirname) {
   if (dirname === "src/components") return "component"
   else if (dirname === "src/layouts") return "layout"
   else if (dirname === "src/articles") return "article"
+  else if (dirname.startsWith("src/articles/")) return "article"
   else if (dirname === "src") return "page"
   else return "unkown"
 }
