2e110776d0fcac5c8e28c8dcc481c96cee498c42
Tsutomu Kuroda <t-kuroda@oiax.jp>
2023-02-22 00:25:29 +0900

    Add tests to check if Japanese text is handled correctly

--- packages/tgweb/test/tgweb/get_site_data.test.mjs
+++ packages/tgweb/test/tgweb/get_site_data.test.mjs
@@ -103,6 +103,18 @@ describe("getSiteData", () => {
     assert.equal(siteData.properties["title"], "No Title")
     assert.equal(siteData.properties["data-current-year"], 2023)
   })
+
+  it("should return the site data with Japanese text", () => {
+    const wd = PATH.resolve(__dirname, "../examples/site_2")
+    const siteData = getSiteData(wd)
+
+    const page = siteData.pages.find(page => page.path == "index_ja.html")
+    assert.equal(page.frontMatter["title"], "ホーム")
+
+    const comp = siteData.components.find(comp => comp.path == "world.html")
+
+    assert(comp.dom.window.document.body.outerHTML.includes("世界"))
+  })
 })
 
 describe("updateSiteData", () => {
