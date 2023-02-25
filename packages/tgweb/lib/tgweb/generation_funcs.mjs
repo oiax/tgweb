70c130d335c6ffd4393b8f1b3a7a2425ecd8a877
Tsutomu Kuroda <t-kuroda@oiax.jp>
2023-02-25 18:07:31 +0900

    Refactoring: extract renderHTML() from generation_funcs.mjs

--- packages/tgweb/lib/tgweb/generation_funcs.mjs
+++ packages/tgweb/lib/tgweb/generation_funcs.mjs
@@ -1,15 +1,13 @@
 import pretty from "pretty"
-import { JSDOM } from "jsdom"
 import getTag from "./get_tag.mjs"
 import filterArticles from "./filter_articles.mjs"
 import { setAttrs } from "./set_attrs.mjs"
-import { removeTgAttributes } from "./remove_tg_attributes.mjs"
 import { getWrapper } from "./get_wrapper.mjs"
 import { makeLocalFrontMatter } from "./make_local_front_matter.mjs"
-import { processTgLinks } from "./process_tg_links.mjs"
 import { expandClassAliases } from "./expand_class_aliases.mjs"
 import { getTitle } from "./get_title.mjs"
 import { sortArticles } from "./sort_articles.mjs"
+import { renderHTML } from "./render_html.mjs"
 
 const generationFuncs = {}
 
@@ -348,75 +346,5 @@ const embedLinksToArticles = (template, node, siteData, path) => {
   Array.from(node.querySelectorAll("tg-links")).forEach(target => target.remove())
 }
 
-const renderHTML = (template, root, siteData, headAttrs, path) => {
-  const dom = new JSDOM(siteData.documentTemplate.serialize())
-
-  processTgLinks(root, siteData, path)
-  removeTgAttributes(root)
-
-  dom.window.document.body.replaceWith(root)
-
-  if (headAttrs["title"])
-    dom.window.document.head.querySelector("title").textContent = headAttrs["title"]
-
-  const link = dom.window.document.head.querySelector("link")
-
-  Object.keys(template.frontMatter).forEach(key => {
-    if (key.startsWith("meta-")) {
-      const name = key.slice(5)
-      const content = template.frontMatter[key]
-      const meta = dom.window.document.createElement("meta")
-      meta.setAttribute("name", name)
-      meta.setAttribute("content", content)
-      link.before(meta)
-    }
-  })
-
-  Object.keys(template.frontMatter).forEach(key => {
-    if (key.startsWith("http-equiv-")) {
-      const name = key.slice(11)
-      const content = template.frontMatter[key]
-      const meta = dom.window.document.createElement("meta")
-      meta.setAttribute("http-equiv", name)
-      meta.setAttribute("content", content)
-      link.before(meta)
-    }
-  })
-
-  Object.keys(template.frontMatter).forEach(key => {
-    if (key.startsWith("property-")) {
-      const name = key.slice(9)
-      const content = template.frontMatter[key]
-
-      const converted = content.replaceAll(/\$\{([^}]+)\}/g, (_, propName) => {
-        if (Object.hasOwn(template.frontMatter, propName)) {
-          return template.frontMatter[propName]
-        }
-        else {
-          `\${${propName}}`
-        }
-      })
-
-      const meta = dom.window.document.createElement("meta")
-      meta.setAttribute("property", name)
-      meta.setAttribute("content", converted)
-      link.before(meta)
-    }
-  })
-
-  Object.keys(template.frontMatter).forEach(key => {
-    if (key.startsWith("link-")) {
-      const rel = key.slice(5)
-      if (rel == "stylesheet") return
-      const href = template.frontMatter[key]
-      const newLink = dom.window.document.createElement("link")
-      newLink.setAttribute("rel", rel)
-      newLink.setAttribute("href", href)
-      link.before(newLink)
-    }
-  })
-
-  return pretty(dom.serialize(), {ocd: true})
-}
 
 export default generationFuncs
---
+++ packages/tgweb/lib/tgweb/render_html.mjs
@@ -0,0 +1,77 @@
+import { JSDOM } from "jsdom"
+import pretty from "pretty"
+import { processTgLinks } from "./process_tg_links.mjs"
+import { removeTgAttributes } from "./remove_tg_attributes.mjs"
+
+const renderHTML = (template, root, siteData, headAttrs, path) => {
+  const dom = new JSDOM(siteData.documentTemplate.serialize())
+
+  processTgLinks(root, siteData, path)
+  removeTgAttributes(root)
+
+  dom.window.document.body.replaceWith(root)
+
+  if (headAttrs["title"])
+    dom.window.document.head.querySelector("title").textContent = headAttrs["title"]
+
+  const link = dom.window.document.head.querySelector("link")
+
+  Object.keys(template.frontMatter).forEach(key => {
+    if (key.startsWith("meta-")) {
+      const name = key.slice(5)
+      const content = template.frontMatter[key]
+      const meta = dom.window.document.createElement("meta")
+      meta.setAttribute("name", name)
+      meta.setAttribute("content", content)
+      link.before(meta)
+    }
+  })
+
+  Object.keys(template.frontMatter).forEach(key => {
+    if (key.startsWith("http-equiv-")) {
+      const name = key.slice(11)
+      const content = template.frontMatter[key]
+      const meta = dom.window.document.createElement("meta")
+      meta.setAttribute("http-equiv", name)
+      meta.setAttribute("content", content)
+      link.before(meta)
+    }
+  })
+
+  Object.keys(template.frontMatter).forEach(key => {
+    if (key.startsWith("property-")) {
+      const name = key.slice(9)
+      const content = template.frontMatter[key]
+
+      const converted = content.replaceAll(/\$\{([^}]+)\}/g, (_, propName) => {
+        if (Object.hasOwn(template.frontMatter, propName)) {
+          return template.frontMatter[propName]
+        }
+        else {
+          `\${${propName}}`
+        }
+      })
+
+      const meta = dom.window.document.createElement("meta")
+      meta.setAttribute("property", name)
+      meta.setAttribute("content", converted)
+      link.before(meta)
+    }
+  })
+
+  Object.keys(template.frontMatter).forEach(key => {
+    if (key.startsWith("link-")) {
+      const rel = key.slice(5)
+      if (rel == "stylesheet") return
+      const href = template.frontMatter[key]
+      const newLink = dom.window.document.createElement("link")
+      newLink.setAttribute("rel", rel)
+      newLink.setAttribute("href", href)
+      link.before(newLink)
+    }
+  })
+
+  return pretty(dom.serialize(), {ocd: true})
+}
+
+export { renderHTML }
