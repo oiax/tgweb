c2e0dadc9a7dd3f4d4184558bf6438a4eb011427
Tsutomu Kuroda <t-kuroda@oiax.jp>
2023-02-25 18:46:08 +0900

    Refactoring: extract renderPage() from generation_funcs.mjs

--- packages/tgweb/lib/tgweb/generate_html.mjs
+++ packages/tgweb/lib/tgweb/generate_html.mjs
@@ -1,10 +1,12 @@
 import getType from "./get_type.mjs"
 import generationFuncs from "./generation_funcs.mjs"
+import { renderPage } from "./render_page.mjs"
 
 const generateHTML = (path, siteData) => {
   const type = getType(path)
 
-  if (typeof generationFuncs[type] === "function") return generationFuncs[type](path, siteData)
+  if (type === "page") return renderPage(path, siteData)
+  else if (type === "article") return generationFuncs["article"](path, siteData)
 }
 
 export default generateHTML
--- packages/tgweb/lib/tgweb/generation_funcs.mjs
+++ packages/tgweb/lib/tgweb/generation_funcs.mjs
@@ -1,11 +1,8 @@
 import pretty from "pretty"
-import { setAttrs } from "./set_attrs.mjs"
 import { getWrapper } from "./get_wrapper.mjs"
 import { expandClassAliases } from "./expand_class_aliases.mjs"
 import { getTitle } from "./get_title.mjs"
 import { renderHTML } from "./render_html.mjs"
-import { embedArticles } from "./embed_articles.mjs"
-import { embedArticleLists } from "./embed_article_lists.mjs"
 import { embedLinksToArticles } from "./embed_links_to_articles.mjs"
 import { embedComponents } from "./embed_components.mjs"
 import { applyWrapper } from "./apply_wrapper.mjs"
@@ -24,44 +21,6 @@ const pp = element => {
 // Prevent warnings when functions dbg and pp are not used.
 if (dbg === undefined) { dbg() }
 if (pp === undefined) { pp() }
-
-generationFuncs["page"] = (path, siteData) => {
-  const relPath = path.replace(/^src\/pages\//, "")
-  const page = siteData.pages.find(page => page.path == relPath)
-
-  if (page) {
-    const pageRoot = page.dom.window.document.body.cloneNode(true)
-    setAttrs(pageRoot)
-    expandClassAliases(page.frontMatter, pageRoot)
-
-    embedComponents(page, pageRoot, siteData, path)
-    embedArticles(pageRoot, siteData, path)
-    embedArticleLists(pageRoot, siteData, path)
-    embedLinksToArticles(page, pageRoot, siteData, path)
-
-    const wrapper = getWrapper(siteData, "pages/" + page.path)
-
-    if (wrapper) {
-      const wrapperRoot = applyWrapper(page, pageRoot, wrapper)
-      const headAttrs = { title: getTitle(page, wrapperRoot) }
-      const layoutRoot = applyLayout(wrapper, wrapperRoot, siteData, path)
-      if (layoutRoot) return renderHTML(page, layoutRoot, siteData, headAttrs, path)
-      else console.log("Error")
-    }
-    else if (page.frontMatter["layout"]) {
-      const headAttrs = { title: getTitle(page, pageRoot) }
-      const layoutRoot = applyLayout(page, pageRoot, siteData, path)
-      if (layoutRoot) return renderHTML(page, layoutRoot, siteData, headAttrs, path)
-      else return renderHTML(page, pageRoot, siteData, headAttrs, path)
-    }
-    else {
-      const headAttrs = { title: getTitle(page, pageRoot) }
-      return renderHTML(page, pageRoot, siteData, headAttrs, path)
-    }
-  }
-
-  return pretty(siteData.documentTemplate.serialize())
-}
 
 generationFuncs["article"] = (path, siteData) => {
   const article = siteData.articles.find(article => "src/articles/" + article.path == path)
