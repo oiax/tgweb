622cd1908dbeea089cc4631405101de82e02b3b1
Tsutomu Kuroda <t-kuroda@oiax.jp>
2023-03-20 09:38:07 +0900

    Fix getLayout/3 so that it reads layout name from site.yml

--- packages/tgweb/lib/tgweb/get_layout.mjs
+++ packages/tgweb/lib/tgweb/get_layout.mjs
@@ -1,6 +1,8 @@
 const getLayout = (siteData, mainTemplate, wrapper) => {
   const layoutName =
-    mainTemplate.frontMatter["layout"] || (wrapper && wrapper.frontMatter["layout"])
+    mainTemplate.frontMatter["layout"] ||
+    (wrapper && wrapper.frontMatter["layout"]) ||
+    siteData.properties["layout"]
 
   return siteData.layouts.find(layout => layout.path === `${layoutName}.html`)
 }
