ad02599a92a19fee5b3987bb979e8395e761b243
Tsutomu Kuroda <t-kuroda@oiax.jp>
2023-04-22 15:31:45 +0900

    Fix get_type.mjs so that extname of templates are checked

--- packages/tgweb/lib/tgweb/get_type.mjs
+++ packages/tgweb/lib/tgweb/get_type.mjs
@@ -3,14 +3,15 @@ import * as PATH from "path"
 const getType = path => {
   const dirname = PATH.dirname(path)
   const filename = PATH.basename(path)
+  const ext = PATH.extname(path)
   const shortDirname = dirname.split("/").slice(0, 2).join("/")
 
   if (filename === "_wrapper.html") return "wrapper"
-  else if (shortDirname === "src/segments") return "segment"
-  else if (shortDirname === "src/components") return "component"
-  else if (shortDirname === "src/layouts") return "layout"
-  else if (shortDirname === "src/articles") return "article"
-  else if (shortDirname === "src/pages") return "page"
+  else if (shortDirname === "src/segments" && ext === ".html") return "segment"
+  else if (shortDirname === "src/components" && ext === ".html") return "component"
+  else if (shortDirname === "src/layouts" && ext === ".html") return "layout"
+  else if (shortDirname === "src/articles" && ext === ".html") return "article"
+  else if (shortDirname === "src/pages" && ext === ".html") return "page"
   else if (filename === "site.toml") return "site.toml"
   else if (filename === "color_scheme.toml") return "color_scheme.toml"
   else return "unkown"
