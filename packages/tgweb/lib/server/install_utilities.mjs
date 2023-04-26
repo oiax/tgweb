1bf5d37e02f83255677d25d597a167066a015cc3
Tsutomu Kuroda <t-kuroda@oiax.jp>
2023-04-26 13:26:07 +0900

    Move {stubs,resources}/tgweb_utilities.js

--- packages/tgweb/lib/server/install_utilities.mjs
+++ packages/tgweb/lib/server/install_utilities.mjs
@@ -3,7 +3,7 @@ import fs from "fs"
 
 const installUtilities = (workingDir) => {
   const packageDir = PATH.resolve(PATH.join(workingDir, "node_modules", "tgweb"))
-  const srcPath = PATH.join(packageDir, "stubs", "tgweb_utilities.js")
+  const srcPath = PATH.join(packageDir, "resources", "tgweb_utilities.js")
   const targetDirPath = PATH.join(process.cwd(), "dist", "js")
   const destPath = PATH.join(process.cwd(), "dist", "js", "tgweb_utilities.js")
   fs.mkdirSync(targetDirPath, { recursive: true })
